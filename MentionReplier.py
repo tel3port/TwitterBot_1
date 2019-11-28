2c9751dc1ca6e21fd9e74ed47203f918420078e6
tel3port <carn3lian@gmail.com>
2019-11-28 04:55:40 +0300

    latest commit

--- MentionReplier.py
+++ MentionReplier.py
@@ -1,4 +1,32 @@
 import tweepy
+import globals as gls
 
 # to reply to everyone that mentions me
+
+
+VALUE_HOLDER_FILE = 'last_seen_id.txt'
+
+
+def get_last_seen_id(my_file):
+    f_read = open(my_file, "r")
+    last_seen_id = int(f_read.read().strip())
+    f_read.close()
+    return last_seen_id
+
+
+def save_last_seen_id(my_id, my_file):
+    f_write = open(my_file, 'w')
+    f_write.write(str(my_id))
+    f_write.close()
+    return
+
+
+mentions = gls.api.mentions_timeline()
+
+print(mentions[0].__dict__.keys())  # converts list into dict and extracts all the keys
+
+print(mentions[0].text)
+
+for single_mention in mentions:
+    print(f"{single_mention.id} - {single_mention.text}")
 
