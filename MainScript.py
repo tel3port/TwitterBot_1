2d02cb59917e3291167bc86226a2de63a2b749ad
tel3port <carn3lian@gmail.com>
2019-12-08 09:08:40 +0300

    almost final commit

--- MainScript.py
+++ MainScript.py
@@ -8,13 +8,14 @@ from ScreenNameFollower import *
 
 
 # for putting everything together... somehow
-# todo put everything together in this  file
+# todo put everything together in this file
 # todo finally deploy on two twitter accounts
 
 custom_joke_list = []
 custom_thnx_list = []
 custom_facts_list = []
 
+# open csv and populate above lists
 first_line = True
 try:
     with open("tweets.csv", "r") as rdr:
@@ -26,12 +27,8 @@ try:
             custom_joke_list.append(single_row[0])
             custom_thnx_list.append(single_row[1])
             custom_facts_list.append(single_row[2])
-
-
 except IOError:
     print("problem reading the csv")
-
-
 finally:
     pass
 
@@ -40,26 +37,30 @@ twitDl_1 = td.TwitDloader(hash_tag='FridayMotivation', count_num=1500, language=
 twitDl_1.tweet_list_downloader()
 print("DONE with tweet extraction")
 
-# send these direct messages to everyone that follows the screen name
-dm_1 = DMSlider(follower_id_list=[], screen_name_list=[], screen_name="GikSoundz", custom_msg_list=custom_msg_list)
-dm_1.follower_extractor()
-dm_1.follower_looper()
-
-mention_replier_1 = MentionsRepr(value_holder_file='last_seen_id.txt', hash_tag='#FridayMotivation', custom_message_list=custom_joke_list)
-mention_replier_1.custom_replier()
-mention_replier_1.shoot_them_up()
-
 # replies to everyone in the csv
 twt_replier = TwitReplier(screen_name_list=[], tweet_id_list=[], custom_tweet_list=custom_facts_list)
 twt_replier.tweet_reader()
 twt_replier.screen_name_follower()
+
+# follow everyone with the provided handle
+handle_follower = HandleFollower(screen_name_list=[], tweets_list_csv="hashtag_tweets.csv", action="r")
+handle_follower.hashtag_tweet_reader()
+handle_follower.twitter_user_follower()
+
+
+# send these direct messages to everyone that follows the screen name
+dm_1 = DMSlider(follower_id_list=[], screen_name_list=[], screen_name="GikSoundz", custom_msg_list=custom_thnx_list)
+dm_1.follower_extractor()
+dm_1.follower_looper()
+
+# reply to all mentions and adds the given hashtag
+mention_replier_1 = MentionsRepr(value_holder_file='last_seen_id.txt', hash_tag='#FridayMotivation', custom_message_list=custom_joke_list)
+mention_replier_1.custom_replier()
+mention_replier_1.shoot_them_up()
+
 
 # tweet on a given hashtag
 hashtag_twtr = TwitOnHashTag(tweet_list_csv="tweets_for_today.csv", action="r", tweets_list=[], hashtag="#CashAppChillFriday")
 hashtag_twtr.tweet_reader()
 hashtag_twtr.tweet_sender()
 
-# follow everyone with the provided handle
-handle_follower = HandleFollower(screen_name_list=[], tweets_list_csv="hashtag_tweets.csv", action="r")
-handle_follower.hashtag_tweet_reader()
-handle_follower.twitter_user_follower()
